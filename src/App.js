43b34fe78ea6d40f0e15052eb69e9d9410a67e55
Andy Baek <andybaek95@gmail.com>
2018-08-01 01:02:38 -0400

    Add Movie Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import ListScreen from './components/ListScreen.js';
-import HomeScreen from './components/HomeScreen.js';
-
+import ListScreen from "./components/ListScreen.js";
+import HomeScreen from "./components/HomeScreen.js";
+import AddMovieScreen from "./components/AddMovieScreen.js";
 
 class App extends Component {
   render() {
@@ -13,6 +13,7 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/add" component={AddMovieScreen} />
             <Route path="/list/:user/:category" component={ListScreen} />
           </Switch>
         </Router>
---
+++ src/components/AddMovieScreen.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
+import ReactStars from "react-stars";
+
+class AddMovieScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      rating: -1,
+      showError: false,
+    };
+  }
+
+  addMovie = () => {
+    const { title, rating } = this.state;
+    console.log(title);
+    console.log(rating);
+    if (title === "" || rating === -1) {
+      this.setState({ showError: true });
+    } else {
+      const data = { title: title, rating: rating };
+      fetch("http://0.0.0.0:5000/add_movie", {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      this.setState({ showError: false, title: "", rating: -1 });
+    }
+  };
+
+  titleChanged = e => {
+    console.log(e.target.value);
+    this.setState({ title: e.target.value });
+  };
+
+  ratingChanged = newRating => {
+    this.setState({ rating: newRating });
+  };
+
+  render() {
+    const { title, rating } = this.state;
+
+    return (
+      <div>
+        <div>Add Movie:</div>
+        <input value={title} onChange={this.titleChanged} />
+        <div className={css(styles.middle)}>
+          <ReactStars
+            count={10}
+            onChange={this.ratingChanged}
+            size={24}
+            half={false}
+            color2={"#ffd700"}
+            value={rating}
+          />
+        </div>
+        <button onClick={() => this.addMovie()}>Add</button>
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  middle: {
+    display: "flex",
+    justifyContent: "center",
+  },
+});
+
+export default AddMovieScreen;
